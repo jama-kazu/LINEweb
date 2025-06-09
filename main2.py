0c08361db353d0690a1ebe690b2338d207a0e0bd
jama-kazu <sgt.kazuhisa.07@gmail.com>
2025-06-09 20:37:59 +0900

    pdf_url

--- main2.py
+++ main2.py
@@ -101,9 +101,8 @@ def main(request):
     pdf_url = ""
     # 3週間前まで試行
     for i in range(3):
-        check_date = today - timedelta(weeks=i) 
-        
-        pdf_url = generate_and_test_menu_url(check_date)
+        target_date = today - timedelta(weeks=i)
+        pdf_url = generate_and_test_menu_url(target_date)
         print(f"URLを試行中: {pdf_url}")
 
         try:
